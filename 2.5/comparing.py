f4de7bc35598e6d8fb3c87b85cee09597ff889a2
daichilee <2033589@learn.vsb.bc.ca>
2025-10-07 22:36:28 +0000

    code

--- 2.5/comparing.py
+++ 2.5/comparing.py
@@ -11,10 +11,30 @@ Test as you go! Describe in your comments what steps you took to test your code.
 
 
 file = open("2.4/responses.csv")
+data = file.readlines()
 
+print("This program is used to check if two people like the same sport")
+firstPerson = input("What is the first person you want to input?")
+firstSport = None
+for line in data[1:]:
+    info = line.strip().split(",")
+    if info[1].strip().lower() == firstPerson.strip().lower():
+        firstSport = info[4].strip()
+        break
 
-for line in file:
-    if "Daichi Lee" in line:
+secondPerson = input("What is the second person you want to input?")
+secondSport = None
+for line in data[1:]:
+    info = line.strip().split(",")
+    if info[1].strip().lower() == secondPerson.strip().lower():
+        secondSport = info[4].strip()
+        break
+
+if firstSport == secondSport:
+    print("You guys like the same sport!")
+else:
+    print("You guys do not like the same sport!")
+
         
 
 
