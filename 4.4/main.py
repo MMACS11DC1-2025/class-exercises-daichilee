81402026009186eea852adc640b302abcc9edb49
daichilee <2033589@learn.vsb.bc.ca>
2025-11-04 22:55:50 +0000

    s

--- 4.4/main.py
+++ 4.4/main.py
@@ -20,7 +20,7 @@ def recursive(t, num_squares, side, rotation):
     recursive(t, num_squares - 1, side + 1, rotation)
 
 num_squares = input("How many squares to draw? Default is 1: ").strip()
-if num_squares == "" or num_squares != num_squares.isdigit:
+if num_squares == "" or num_squares or not num_squares.isdigit:
     num_squares = 1
 else:
     num_squares = max(1, min(int(num_squares), 1000))
