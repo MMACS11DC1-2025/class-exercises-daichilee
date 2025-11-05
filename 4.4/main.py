aacda04ec5b576b3a87e50c7eaf9e6158222720e
daichilee <2033589@learn.vsb.bc.ca>
2025-11-05 22:46:55 +0000

    comments

--- 4.4/main.py
+++ 4.4/main.py
@@ -1,15 +1,19 @@
 import turtle
 
+#setup turtle
 t = turtle.Turtle()
 t.hideturtle()
 t.speed(0)
 
+#function to draw squares
 def draw_square(t, side):
     for _ in range(4):
         t.forward(side)
         t.left(90)
 
+#recursive function that adds 1 to side and -1 to number of times will draw per loop. goes back to center every time
 def recursive(t, num_squares, side, rotation):
+    #end case stops here
     if num_squares == 0:
         return
     t.penup()
@@ -19,8 +23,8 @@ def recursive(t, num_squares, side, rotation):
     t.right(rotation)
     recursive(t, num_squares - 1, side + 1, rotation)
 
+#get user input
 num_squares = input("How many squares to draw? Default is 1: ").strip()
-
 if num_squares == "" or not num_squares.isdigit():
     num_squares = 1
 else:
@@ -29,11 +33,13 @@ else:
         num_squares = 1000
     elif num_squares < 1:
         num_squares = 1
-
+#check to see if program gets right input also just for user clarification
 print("You inputted " + str(num_squares))
-  
+
+#make list of valid colors
 valid_colors = ["red", "blue", "green", "yellow", "orange", "purple", "pink", "black", "white"]
 color = ""
+#while loop to check for right color
 while color not in valid_colors:
     color = input("What color do you want the squares? Default is blue: ").strip()
     if color == "":
@@ -41,11 +47,13 @@ while color not in valid_colors:
     if color not in valid_colors:
         print("Invalid color! Valid colors are:", valid_colors)
 
+#settings
 side = 1
 rotation = 15
 
 t.color(color)
 
+#call recusive function
 recursive(t, num_squares, side, rotation)
 turtle.done()
 
