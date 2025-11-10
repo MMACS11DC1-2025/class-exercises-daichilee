47438ab6aa0bd59648140d03b99c4e4849474da0
daichilee <2033589@learn.vsb.bc.ca>
2025-11-10 00:11:18 +0000

    changed settings to dictionary

--- 4.4/main.py
+++ 4.4/main.py
@@ -5,27 +5,33 @@ t = turtle.Turtle()
 t.hideturtle()
 t.speed(0)
 
-
 #function to draw squares
 def draw_square(t, side):
     for _ in range(4):
         t.forward(side)
         t.left(90)
 
-#recursive function that adds 1 to side and -1 to number of times will draw per loop. goes back to center every time
+#recursive function that adds 1 to side and -1 to number of times will draw per loop to get down to base case. Goes back to center every time
 def recursive(t, num_squares, side, rotation):
     #end case stops here
     if num_squares == 0:
         return 0
-    
     t.penup()
     t.goto(0, 0)
     t.pendown()
-    #calls draw_square to draw square per recursion then modifies each one via size and rotation
+    #calls draw_square to draw square per recursion then modifies each one by size and rotation
     draw_square(t, side)
     t.right(rotation)
     #adds one to count for total recursion calls
     return 1+ recursive(t, num_squares - 1, side + 1, rotation)
+
+#make dictionary
+settings = {
+    'default_color': 'blue',
+    'side': 1,
+    'rotation': 15,
+    'valid_colors': ["red", "blue", "green", "yellow", "orange", "purple", "pink", "black", "white"]
+}
 
 #get user input
 num_squares = input("How many squares to draw? Default is 1: ").strip()
@@ -42,24 +48,19 @@ else:
 print("You inputted " + str(num_squares))
 
 #make list of valid colors
-valid_colors = ["red", "blue", "green", "yellow", "orange", "purple", "pink", "black", "white"]
 color = ""
 #while loop to check for right color
-while color not in valid_colors:
-    color = input("What color do you want the squares? Default is blue: ").strip()
+while color not in settings['valid_colors']:
+    color = input("What color do you want the squares? Default is " + settings['default_color'] + ": ").strip()
     if color == "":
-        color = "blue"
-    if color not in valid_colors:
-        print("Invalid color! Valid colors are:", valid_colors)
+        color = settings['default_color']
+    if color not in settings['valid_colors']:
+        print("Invalid color! Valid colors are:", settings['valid_colors'])
 
-#settings
-side = 1
-rotation = 15
-
+#set turtle to inputed color
 t.color(color)
 
-#print total recursion calls and calls function
-numRecursion = recursive(t, num_squares, side, rotation)
+#print total recursion calls by return + 1 value and calls function in one line
+numRecursion = recursive(t, num_squares, settings['side'], settings['rotation'])
 print("Total recursive calls: " + str(numRecursion))
 turtle.done()
-
