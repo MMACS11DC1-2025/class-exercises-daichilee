3762a0855a44532498bd9365ba489c37014cfdeb
daichilee <2033589@learn.vsb.bc.ca>
2025-11-18 20:25:20 +0000

    htingyasasaaawaw

--- 5.1/image_open.py
+++ 5.1/image_open.py
@@ -1,4 +1,11 @@
 from PIL import Image
 
-image_green = Image.open("CS11/5.1/kid-green.jpg").load()
-image_beach = Image.open("CS11/5.1/beach.jpg").load()
+def isGreen(r,g,b):
+    if r < 25 and r > 0 and g > 255 and b < 36 and b > 0:
+        return True
+    return False
+
+
+
+image_green = Image.open("5.1/kid-green.jpg").load()
+image_beach = Image.open("5.1/beach.jpg").load()
