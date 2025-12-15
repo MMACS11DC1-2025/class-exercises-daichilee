dc445ef23520451c4fb68e4c60551fd695059aef
daichilee <2033589@learn.vsb.bc.ca>
2025-12-15 20:42:21 +0000

    fix

--- 6.7/main.py
+++ 6.7/main.py
@@ -1,7 +1,7 @@
 from PIL import Image
 
 img = (
-"red1.jpg", "red2.webp", "red3.webp", "red4.webp", "red5.jpg", "red6.avif", "red7.avif", "red8.jpg", "red9.jpg", "red10.webp"
+"red1.jpg", "red2.jpeg", "red3.jpg", "red4.jpeg", "red5.jpg"
 )
 
 
