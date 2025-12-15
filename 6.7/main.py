f1a50598c523c7840e8d4aa7e0650bb6a1d1d960
daichilee <2033589@learn.vsb.bc.ca>
2025-12-15 20:53:44 +0000

    imagesa

--- 6.7/main.py
+++ 6.7/main.py
@@ -1,7 +1,23 @@
 from PIL import Image
+import time
 
-img = (
-"red1.jpg", "red2.jpeg", "red3.jpg", "red4.jpeg", "red5.jpg"
+
+def isRed(r, g, b):
+    if r > 150 and g < 100 and b < 100:
+        return True
+    else:
+        return False
+
+imgList = (
+"red1.jpg", "red2.jpeg", "red3.jpg", "red4.jpeg", "red5.jpg", "yellow1.jpg", "yellow2.jpg", "green1.jpg", "green2.jpg", "green3.jpg"
 )
 
+startTimer = time.time()
 
+for image in imgList:
+    file = image.open("6.7/images/" + image)
+    img = file.load()
+    width = file.width
+    height = file.height
+
+    redPixelCount = []
